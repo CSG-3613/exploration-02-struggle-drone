421b89eb0bf3c84aaba02943c2ea0c2e46074092
beo8538 <beo8538@utulsa.edu>
2023-03-02 11:00:37 -0600

    update3

--- UAV_In_Bound/Assets/ExampleAssets/Scripts/InputManager.cs
+++ UAV_In_Bound/Assets/ExampleAssets/Scripts/InputManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.MagicLeap;
 
 public class InputManager : MonoBehaviour
 /**
@@ -15,6 +17,8 @@ public class InputManager : MonoBehaviour
 
     private List<ARRaycastHit> _hits = new List<ARRaycastHit>();
 
+    private Touch touch;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,14 +28,30 @@ public class InputManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        touch = Input.GetTouch(0);
+
+        if(Input.touchCount <= 0 || touch.phase != TouchPhase.Began)
         {
+            return;
+        }
+
+        if (IsPointerOverUI(touch)) return;
+
             Ray ray = arCam.ScreenPointToRay(Input.mousePosition); //reads "mouse" position
             if(_raycastManager.Raycast(ray, _hits))
             {
                 Pose pose = _hits[0].pose;
                 Instantiate(DataHandler.Instance.objects, pose.position, pose.rotation);
             }
-        }
     }
+
+    bool IsPointerOverUI(Touch touch)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touch.position.x, touch.position.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0;
+    }
+
 }
